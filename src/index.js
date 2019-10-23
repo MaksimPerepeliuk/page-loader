0d46fa1dd90c72475af56a5e1a7acc54151390de
Max Perepelyuk <maksim.perepelyuk@mail.ru>
2019-10-23 19:21:08 +0300

    created function changeLinks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { promises as fs } from 'fs';
 import url from 'url';
+import cheerio from 'cheerio';
 
 const makeName = (adress) => {
   const { hostname, pathname } = url.parse(adress);
@@ -8,6 +9,26 @@ const makeName = (adress) => {
   return `${name}.html`;
 };
 
+const isLocalLinks = (link) => (link ? !link.includes('https', 0) && !link.includes('http', 0) : false);
+
+const makeNameForLinks = (link) => link.split('/').join('-');
+
+const tagExtraction = (html) => {
+  const dom = cheerio.load(html);
+  const elementsWithLinks = dom('link').add('img[src]');
+  ['src', 'href'].forEach((attribute) => {
+    elementsWithLinks.attr(attribute, (i, elem) => (
+      isLocalLinks(elem) ? makeNameForLinks(elem) : elem
+    ));
+  });
+  return dom.html();
+};
+
 export default (adress, outputDir) => axios.get(adress)
   .then((page) => fs.writeFile(`${outputDir}/${makeName(adress)}`, page.data))
   .catch(console.log);
+
+
+fs.readFile('__tests__/__fixtures__/test.html', 'utf-8')
+  .then((data) => tagExtraction(data))
+  .then(console.log);
