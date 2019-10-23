2fbdf15b37147e75d9a88a4e2d67036c42c442ad
Max Perepelyuk <maksim.perepelyuk@mail.ru>
2019-10-23 21:56:41 +0300

    changed load function

--- src/index.js
+++ src/index.js
@@ -3,32 +3,44 @@ import { promises as fs } from 'fs';
 import url from 'url';
 import cheerio from 'cheerio';
 
-const makeName = (adress) => {
-  const { hostname, pathname } = url.parse(adress);
+const isLocalLink = (link) => (link ? !link.includes('https', 0) && !link.includes('http', 0) : false);
+
+const makeName = (link) => {
+  if (isLocalLink(link)) {
+    return link.split('/').join('-');
+  }
+  const { hostname, pathname } = url.parse(link);
   const name = `${hostname}.${pathname.slice(1)}`.split('.').join('-');
   return `${name}.html`;
 };
 
-const isLocalLinks = (link) => (link ? !link.includes('https', 0) && !link.includes('http', 0) : false);
-
-const makeNameForLinks = (link) => link.split('/').join('-');
-
-const tagExtraction = (html) => {
+const getLocalLinks = (html) => {
+  const localLinks = [];
   const dom = cheerio.load(html);
-  const elementsWithLinks = dom('link').add('img[src]');
-  ['src', 'href'].forEach((attribute) => {
-    elementsWithLinks.attr(attribute, (i, elem) => (
-      isLocalLinks(elem) ? makeNameForLinks(elem) : elem
-    ));
-  });
-  return dom.html();
+  const elementsWithLinks = dom('link').add('img[src]').add('script');
+  elementsWithLinks.attr('src', (i, elem) => (isLocalLink(elem) ? localLinks.push(elem) : null));
+  elementsWithLinks.attr('href', (i, elem) => (isLocalLink(elem) ? localLinks.push(elem) : null));
+  return localLinks;
 };
 
-export default (adress, outputDir) => axios.get(adress)
-  .then((page) => fs.writeFile(`${outputDir}/${makeName(adress)}`, page.data))
-  .catch(console.log);
+const load = (adress, outputDir) => {
+  const filepath = `${outputDir}/${makeName(adress)}`;
+  return axios.get(adress)
+    .then((page) => fs.writeFile(filepath, page.data))
+    .then(() => fs.readFile(filepath, 'utf-8'))
+    .then((html) => getLocalLinks(html))
+    .then((links) => links.map(link => ))
+    .catch(console.log);
+};
 
-
-fs.readFile('__tests__/__fixtures__/test.html', 'utf-8')
-  .then((data) => tagExtraction(data))
-  .then(console.log);
+load('https://hexlet.io/courses', '/tmp');
+// const changeLinks = (html) => {
+//   const dom = cheerio.load(html);
+//   const elementsWithLinks = dom('link').add('img[src]');
+//   ['src', 'href'].forEach((attribute) => {
+//     elementsWithLinks.attr(attribute, (i, elem) => (
+//       isLocalLink(elem) ? makeNameForLinks(elem) : elem
+//     ));
+//   });
+//   return dom.html();
+// };
