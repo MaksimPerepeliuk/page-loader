ee41b662657ec557ae1b8fcd9aad45fcc66c0d49
Max Perepelyuk <kostin7778@gmail.com>
2019-11-04 22:49:12 +0300

    updated makeNames functions

--- src/index.js
+++ src/index.js
@@ -7,32 +7,29 @@ import path from 'path';
 
 const log = debug('page-loader');
 
-const isLocalLink = (link) => (link ? !link.includes('https://', 0) && !link.includes('http://', 0) : false);
-
 const getOriginUrl = (adress) => `${url.parse(adress).protocol}//${url.parse(adress).hostname}`;
 
-const urlWithoutProtocol = (adress) => `${url.parse(adress).hostname}${url.parse(adress).pathname}`;
-
-const makeName = (pathTo, type) => {
-  switch (type) {
-    case 'html':
-      return `${pathTo.split('.').join('-').split('/').join('-')}.html`;
-    case 'dir':
-      return `${pathTo.split('.').join('-').split('/').join('-')}_files`;
-    default:
-      return `/${pathTo.split('/').join('-')}`;
-  }
+const endings = {
+  htmlFile: '.html',
+  directory: '_files',
 };
 
-const makePath = (dirPath, name) => path.join(dirPath, name);
+const makeNameFromUrl = (urlAdress, type) => {
+  const urlWithoutProtocol = `${url.parse(urlAdress).hostname}${url.parse(urlAdress).pathname}`;
+  return `${urlWithoutProtocol.split('.').join('-').split('/').join('-')}${endings[type]}`;
+};
 
-const adressAttrs = ['src', 'href'];
+const makeNameFromLocalLink = (link) => `/${link.split('/').join('-')}`;
+
+const isLocalLink = (link) => link && !url.parse(link).hostname;
+
+const adressAttributes = ['src', 'href'];
 
 const getLocalLinks = (html) => {
   const localLinks = [];
   const dom = cheerio.load(html);
   const elementsWithLinks = dom('link').add('img[src]').add('script');
-  adressAttrs.forEach((attr) => (
+  adressAttributes.forEach((attr) => (
     elementsWithLinks.attr(attr, (i, elem) => (isLocalLink(elem) ? localLinks.push(elem) : null))));
   return localLinks;
 };
@@ -40,10 +37,10 @@ const getLocalLinks = (html) => {
 const changeLocalLinks = (dirName, html) => {
   const dom = cheerio.load(html);
   const elementsWithLinks = dom('link').add('img[src]').add('script');
-  adressAttrs.forEach((attr) => elementsWithLinks.attr(attr, (i, link) => {
+  adressAttributes.forEach((attr) => elementsWithLinks.attr(attr, (i, link) => {
     if (isLocalLink(link)) {
-      const filePath = makePath(dirName, makeName(link.slice(1)));
-      log(`changing the path of a local resource from ${link} to ${filePath}`);
+      const filePath = path.join(dirName, makeNameFromLocalLink(link.slice(1)));
+      log(`4. changing the path of a local resource from ${link} to ${filePath}`);
       return filePath;
     }
     return null;
@@ -53,7 +50,7 @@ const changeLocalLinks = (dirName, html) => {
 
 const loadLocalResources = (links, adress) => links.map((link) => {
   const urlLink = `${adress}${link}`;
-  log(`start loading local resource at ${urlLink}`);
+  log(`2. start loading content by link ${urlLink}`);
   return axios({
     method: 'get',
     url: urlLink,
@@ -62,10 +59,10 @@ const loadLocalResources = (links, adress) => links.map((link) => {
 });
 
 export default (adress, outputDir) => {
-  log(`start loading page at URL ${adress} and save it in directory ${outputDir}`);
+  log(`1. start loading page at URL ${adress} and save it in directory ${outputDir}`);
   let html;
-  const mainFilePath = makePath(outputDir, makeName(urlWithoutProtocol(adress), 'html'));
-  const localFilesDir = makePath(outputDir, makeName(urlWithoutProtocol(adress), 'dir'));
+  const htmlFilePath = path.join(outputDir, makeNameFromUrl(adress, 'htmlFile'));
+  const localFilesDir = path.join(outputDir, makeNameFromUrl(adress, 'directory'));
   return axios.get(adress)
     .then((page) => {
       html = page.data;
@@ -74,12 +71,12 @@ export default (adress, outputDir) => {
     .then(() => loadLocalResources(getLocalLinks(html), getOriginUrl(adress)))
     .then((promises) => Promise.all(promises))
     .then((contents) => contents.map((content) => {
-      const pathname = url.parse(content.config.url).pathname.slice(1);
-      const filePath = makePath(localFilesDir, makeName(pathname, 'link'));
-      log(`loading content by local link - ${content.config.url} and save it in directory - ${localFilesDir}`);
+      const localLink = url.parse(content.config.url).pathname.slice(1);
+      const filePath = path.join(localFilesDir, makeNameFromLocalLink(localLink));
+      log(`3. content from ${content.config.url} save to ${localFilesDir}`);
       return fs.writeFile(filePath, content.data);
     }))
-    .then(() => changeLocalLinks(makeName(urlWithoutProtocol(adress), 'dir'), html))
-    .then((newHtml) => fs.writeFile(mainFilePath, newHtml))
-    .then(() => log(`write html file to ${mainFilePath}`));
+    .then(() => changeLocalLinks(makeNameFromUrl(adress, 'directory'), html))
+    .then((newHtml) => fs.writeFile(htmlFilePath, newHtml))
+    .then(() => log(`5. write html file to ${htmlFilePath}`));
 };
